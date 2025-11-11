539927f89c88038e9bdc7ec0063916ffffa8eb1b
JFrnck <jeanfranckqv@gmail.com>
2025-11-11 16:19:34 -0500

    Actualizar app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,13 +8,15 @@ import { TutorUpdate } from './updates/tutor.update';
 import { VoiceUpdate } from './updates/voice.update';
 import { AppController } from './app.controller';
 
+const TELEGRAM_TOKEN = "8590046011:AAEgCL05S3i23Y3sjwgQQDDKAWRN76TVS6g" 
+
 @Module({
   imports: [
     // Carga .env y lo expone en process.env y ConfigService
     ConfigModule.forRoot({ isGlobal: true }),              // 👈
 
     TelegrafModule.forRoot({
-      token: process.env.TELEGRAM_BOT_TOKEN!,
+      token: process.TELEGRAM_TOKEN!,
       include: []
     })
   ],
